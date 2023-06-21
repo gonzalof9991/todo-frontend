06204d58f9621c9b33fc91d2370756a3d5b6581c
gonzalo9991r <go.rojasf@duocuc.cl>
2023-06-21 16:04:47 -0400

    ADD: integration to search part-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,22 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {},
-    container: {
-      padding: {
+    container:{
+      center: true,
+      screens:{
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      padding:{
         DEFAULT: '1rem',
         sm: '2rem',
         lg: '4rem',
         xl: '5rem',
         '2xl': '6rem',
-      },
+      }
     },
     screens: {
       'xs': '320px',
